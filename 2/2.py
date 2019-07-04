b669e6448e0e655ed1fd2a88928c0744746d27ba
qinlh <musk_qin@163.com>
2019-07-04 08:34:03 +0800

    new 2

--- 2/2.py
+++ 2/2.py
@@ -1,33 +1,32 @@
 #!usr/local/bin/python3
-# -*-coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 
 import random
-from PIL import ImageFont,ImageDraw,Image,ImageFilter
-
+import string
 
 def rndChar():
-    return (chr(random.randint(65,90)))
-
-def randomColor():
-    return (random.randint(32,255),random.randint(32,255),random.randint(32,255))
-
-def randomColor2():
-    return (random.randint(64,255),random.randint(64,255),random.randint(64,255))
+    temp=random.randint(1,100)
+    if temp%2==0:
+        return (chr(random.randint(65,90)))
+    else:
+        return (chr(random.randint(97,122)))
 
 
-height=60
-width=60*4
-image=Image.new('RGB',(width,height),(255,255,255))
-font1=ImageFont.truetype(r'/system/Library/Fonts/Arial.ttf',36)
-draw=ImageDraw.Draw(image)
-for x in range(width):
-    for y in range(height):
-        draw.point((x,y),fill=randomColor())
+def rndNum():
+    return(str(random.randint(0,10)))
 
-for ch in range(4):
-    draw.text((60 * ch+10,10),rndChar(),font=font1,fill=randomColor())
-    # draw.text((60*ch+10,10),charColor(), font=font1, fill=randomColor())
+def generatorStr():
+    str1=""
+    for i in range(25):
+        temp=random.randint(0,20)
+        if temp%2==0:
+            ch1=rndNum()
+        else:
+            ch1=rndChar()
+        str1+=ch1
+    return str1
 
-image=image.filter(ImageFilter.BLUR)
-image.show()
+for i in range(10):
+    print(generatorStr())
+
